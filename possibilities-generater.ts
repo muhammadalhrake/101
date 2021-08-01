182d5fc4edb004d93e1c01d066a0e441b08d9ac8
muhammadalhrake <mohammadalhrake99@gmail.com>
2021-08-01 16:48:08 +0300

    1 file modified

--- possibilities-generater.ts
+++ possibilities-generater.ts
@@ -1,4 +1,9 @@
-import {PossibilityTree}from './101.model'
-function possibilitiesGenerater(possibilityTree:PossibilityTree){
-let f;
+import { PossibilityTree } from './101.model';
+function possibilitiesGenerater(
+  possibilityTree: PossibilityTree,
+  rules: number,
+  digits: number
+) {
+  let fN, sN, mN;
+  fN = sN = mN = 0;
 }
