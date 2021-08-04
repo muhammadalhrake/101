9cf9982dc25cb48c717e7d90ef33e69b5e050aae
muhammadalhrake <mohammadalhrake99@gmail.com>
2021-08-04 11:58:37 +0300

    final difficulty of 101

--- index.ts
+++ index.ts
@@ -103,7 +103,7 @@ function ozoGeneration(
   }
   return generationArr;
 }
-//console.log(ozoGeneration(40,['1'],['4'],['Easy','medium','Difficult']))
+//console.log(ozoGeneration(40,['1'],['1'],['Easy','medium','Difficult']))
 //setInterval(()=>console.log(generateForLastDigits(possibilityArrForMoreThanThreeDigits, 5, 5,'medium')),500)
 //console.log(possibilityTree[1][1]['Difficult'])
 //setInterval(()=>console.log(ozoGeneration(40,['6','7','8','9'],['4','5','6','7','8','9'],['Easy','medium','Difficult'])),500)
--- possibilities-generater.ts
+++ possibilities-generater.ts
@@ -6,6 +6,11 @@ import { ansArray } from './101Anwers';
 function between(min: number, max: number) {
   return random.int(min, max);
 }
+///random for easy digits
+const firstEasyDigits = () => between(1, 4);
+const lastEasyDigits = () => between(0, 4);
+///for difficult digits
+const firstDifficultDigits = () => between(5, 9);
 
 function possibilitiesGenerator(
   possibilityTree: PossibilityTree,
@@ -13,24 +18,49 @@ function possibilitiesGenerator(
   digits: number,
   level: string
 ) {
-  let possibelNumber: string[] = [];
-  for (let i = 1; i <= 9; i++) {
+  //console.log(rules);
+  const ez1 = ['1', '2', '3', '4'];
+  const ez2 = ['0', ...ez1];
+  const hard = ['5', '6', '7', '8', '9'];
+  let generateNum: string[] = [''];
+  if (level == 'Easy') {
     if (digits == 1) {
-      possibelNumber.push(i.toString());
-    } else {
-      for (let j = 0; j <= 9; j++) {
-        if (digits == 2) {
-          possibelNumber.push(i.toString() + j.toString());
-        } else {
-          for (let k = 0; k <= 9; k++) {
-            possibelNumber.push(i.toString() + j.toString() + k.toString());
-          }
-        }
-      }
+      generateNum = ez1;
+    } else if (digits == 2) {
+      let merged = ez1.map(f => ez2.map(s => f + s));
+      generateNum = merged.flat();
+    } else if (digits == 3) {
+      let merged = ez1.map(f => ez2.map(s => ez2.map(th => f + s + th))).flat();
+      generateNum = merged.flat();
+      console.log(generateNum);
+    }
+  } else if (level == 'medium') {
+    if (digits == 1) {
+      generateNum = ez1;
+    } else if (digits == 2) {
+      let merged = hard.map(f => ez2.map(s => f + s));
+      generateNum = merged.flat();
+    } else if (digits == 3) {
+      let merged = hard
+        .map(f => ez2.map(s => hard.map(th => f + s + th)))
+        .flat();
+      generateNum = merged.flat();
+    }
+  } else if (level == 'Difficult') {
+    if (digits == 1) {
+      generateNum = hard;
+    } else if (digits == 2) {
+      let merged = hard.map(f => hard.map(s => f + s));
+      generateNum = merged.flat();
+    } else if (digits == 3) {
+      let merged = hard
+        .map(f => ez2.map(s => ez2.map(th => f + s + th)))
+        .flat();
+      generateNum = merged.flat();
     }
   }
-  //console.log(possibelNumber);
-  possibilityTree[rules][digits][level] = possibelNumber;
+  possibilityTree[rules][digits][level] = generateNum;
+
   return possibilityTree;
 }
 
@@ -40,8 +70,9 @@ export function generateForFirstThreeDigits(
   digits: number,
   level: string
 ) {
-  //console.log(rules, digits, level);
-  possibilityTree[rules][digits][level].splice(0, 1);
+  if (possibilityTree[rules][digits][level][0] == '') {
+    possibilityTree[rules][digits][level].splice(0, 1);
+  }
   if (possibilityTree[rules][digits][level].length == 0) {
     possibilityTree = possibilitiesGenerator(
       possibilityTree,
@@ -50,6 +81,8 @@ export function generateForFirstThreeDigits(
       level
     );
   }
+
+  console.log(possibilityTree);
   let copyArr = {
     possibel: possibilityTree,
     generate: {
@@ -58,19 +91,17 @@ export function generateForFirstThreeDigits(
       secondNumber: 5
     }
   };
-  //console.log(possibilityTree[rules][digits][level])
   try {
     let fNum = +(rules.toString() + '0' + rules.toString());
     let randomSelect = between(
       0,
       possibilityTree[rules][digits][level].length - 1
     );
-    //console.log(possibilityTree[rules][digits][level].length);
     let sNum = possibilityTree[rules][digits][level][randomSelect];
-    //console.log(sNum);
     copyArr.possibel[rules][digits][level] = possibilityTree[rules][digits][
       level
     ].filter(value => value != sNum);
+    console.log(copyArr.possibel[rules][digits][level]);
     copyArr.generate.answers = ansArray(fNum, +sNum);
     copyArr.generate.firstNumber = fNum;
     copyArr.generate.secondNumber = +sNum;
@@ -130,8 +161,3 @@ export function generateForLastDigits(
 
   return copyOf;
 }
-///random for easy digits
-const firstEasyDigits = () => between(1, 4);
-const lastEasyDigits = () => between(0, 4);
-///for difficult digits
-const firstDifficultDigits = () => between(5, 9);
